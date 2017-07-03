2d9a48376678e469e41b0c9ddd91f9e06cc37f84
Illia Tokarenko <illiatokarenko@Illias-MBP.Dlink>
2017-07-04 02:45:14 +0300

    pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'eventurize',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-52-39-196-157.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/eventurize-key-pair.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:illia-to/web-app.git',
+      path: '/home/ubuntu/server',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
