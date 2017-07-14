9c20f7cc6f53134796ba3c1898f55f399125f77a
Illia Tokarenko <illiatokarenko@Illias-MBP.Dlink>
2017-07-15 02:44:34 +0300

    fix root path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/public'));
+
 app.listen(3000, () => console.log('Server running on port 3000'))
